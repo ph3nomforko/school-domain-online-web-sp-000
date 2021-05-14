3ccd0b133f22dcc559eb791cd6af1cce94cbffec
Codi Kraus <codi.kraus@gmail.com>
2021-05-14 01:06:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,13 @@
 # code here!
 class School
-  
+
   def initialize(name)
     @name = name
     @roster = {}
   end
 
   def add_student(name, grade)
-    roster[grade] 
+    roster[grade]
 
   def grade(grade)
     roster[]
